0e71a96695517833c61cc39ad6017345c6ab1a2f
Guilhem Saurel <guilhem.saurel@gmail.com>
2016-05-29 21:22:49 +0200

    case sensitive try first

--- eouimb/backends.py
+++ eouimb/backends.py
@@ -4,22 +4,23 @@ from django.contrib.auth.backends import ModelBackend
 
 class EmailOrUsernameInsensitiveModelBackend(ModelBackend):
     """
-    This backends lets an user authenticate with his username, email
-    or USERNAME_FIELD, case insensitively
+    Backend that authenticates with username or email, case insensitively
     """
     def authenticate(self, username=None, password=None, **kwargs):
         UserModel = get_user_model()  # noqa
-        if username is None:
-            username = kwargs.get(UserModel.USERNAME_FIELD)
+        # username may contain '@', so first try with username
+        # username are case sensitive by default, try case sensitively first
         try:
-            # username may contain '@', so first try with username
-            user = UserModel._default_manager.get(username__iexact=username)
+            user = UserModel._default_manager.get(username=username)
         except:
             try:
-                user = UserModel._default_manager.get(email__iexact=username)
+                user = UserModel._default_manager.get(username__iexact=username)
             except:
-                # Run the default password hasher once to reduce the timing
-                # difference between an existing and a non-existing user (#20760).
-                user = UserModel()
+                try:
+                    user = UserModel._default_manager.get(email__iexact=username)
+                except:
+                    # Run the default password hasher once to reduce the timing
+                    # difference between an existing and a non-existing user (#20760).
+                    user = UserModel()
         if user.check_password(password):
             return user
--- eouimb/tests.py
+++ eouimb/tests.py
@@ -4,14 +4,18 @@ from django.test import TestCase
 
 class EmailOrUsernameInsensitiveModelBackendTests(TestCase):
     def setUp(self):
-        for user, email in [('foo', 'foo@example.org'), ('@nn@', 'anna@example.org')]:
-            User.objects.create_user(user, email=email, password=user)
+        User.objects.create_user('foo', email='foo@example.org', password='foo')
+        User.objects.create_user('@nn@', email='anna@example.org', password='@nn@')
+        User.objects.create_user('@NN@', email='anna@example.fr', password='@NN@')
 
     def test_login(self):
         self.assertTrue(self.client.login(username='foo', password='foo'))
         self.assertTrue(self.client.login(username='fOo', password='foo'))
+        self.assertTrue(self.client.login(username='foo@example.org', password='foo'))
+        self.assertTrue(self.client.login(username='@nn@', password='@nn@'))
+        self.assertFalse(self.client.login(username='@Nn@', password='@nn@'))
+        self.assertFalse(self.client.login(username='@NN@', password='@nn@'))
+        self.assertFalse(self.client.login(username='@nn@', password='@NN@'))
+        self.assertTrue(self.client.login(username='ANNA@example.org', password='@nn@'))
         self.assertFalse(self.client.login(username='ofo', password='foo'))
         self.assertFalse(self.client.login(username='foo', password='fOo'))
-        self.assertTrue(self.client.login(username='foo@example.org', password='foo'))
-        self.assertTrue(self.client.login(username='@Nn@', password='@nn@'))
-        self.assertTrue(self.client.login(username='ANNA@example.org', password='@nn@'))
