8690b3fe8636086d94f1d13c7a37481752c98e4d
Guilhem Saurel <guilhem.saurel@gmail.com>
2016-05-29 19:28:49 +0200

    eou{ → i}mb

--- eoumb/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class EoumbConfig(AppConfig):
-    name = 'eoumb'
--- test_eouimb/wsgi.py
+++ test_eouimb/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for test_eoumb project.
+WSGI config for test_eouimb project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "test_eoumb.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "test_eouimb.settings")
 
 application = get_wsgi_application()
