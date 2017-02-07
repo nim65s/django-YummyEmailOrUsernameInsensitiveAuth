2ba9c52ed08f68d00d11c2e756ad604fbe2b65cc
Élie Bouttier <elie@bouttier.eu>
2017-02-07 12:06:32 +0100

    check user is active

--- yeouia/backends.py
+++ yeouia/backends.py
@@ -8,19 +8,25 @@ class YummyEmailOrUsernameInsensitiveAuth(ModelBackend):
     """
     def authenticate(self, username=None, password=None, **kwargs):
         UserModel = get_user_model()  # noqa
+        if username is None:
+            username = kwargs.get(UserModel.USERNAME_FIELD)
         # username may contain '@', so first try with username
         # username are case sensitive by default, try case sensitively first
+        user = None
         try:
-            user = UserModel._default_manager.get(username=username)
-        except:
+            user = UserModel._default_manager.get_by_natural_key(username)
+        except UserModel.DoesNotExist:
             try:
                 user = UserModel._default_manager.get(username__iexact=username)
-            except:
+            except (UserModel.DoesNotExist, UserModel.MultipleObjectsReturned):
                 try:
                     user = UserModel._default_manager.get(email__iexact=username)
-                except:
-                    # Run the default password hasher once to reduce the timing
-                    # difference between an existing and a non-existing user (#20760).
-                    user = UserModel()
-        if user.check_password(password):
-            return user
+                except (UserModel.DoesNotExist, UserModel.MultipleObjectsReturned):
+                    pass
+        if user:
+            if user.check_password(password) and self.user_can_authenticate(user):
+                return user
+        else:
+            # Run the default password hasher once to reduce the timing
+            # difference between an existing and a non-existing user (#20760).
+            UserModel().set_password(password)
