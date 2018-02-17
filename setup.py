e20af5a42cd46514d3af245b6b11c2e141967f1b
Guilhem Saurel <guilhem.saurel@gmail.com>
2018-02-17 01:05:26 +0100

    v1.1.0

--- setup.py
+++ setup.py
@@ -2,29 +2,33 @@ import os
 
 from setuptools import setup
 
-with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+readme_file = os.path.join(os.path.dirname(__file__), 'README.rst')
+if not os.path.isfile(readme_file):
+    readme_file = os.path.join(os.path.dirname(__file__), 'README.md')
+with open(readme_file) as readme:
     README = readme.read()
+
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-yeouia',
-    version='1.0.0',
+    version='1.1.0',
     packages=['yeouia'],
-    install_requires=['Django>=1.10'],
+    install_requires=['Django>=1.11'],
     include_package_data=True,
-    license='GPL License',
+    license='BSD',
     description='A Django auth backend that works with email or username',
     long_description=README,
-    url='https://saurel.me/',
+    url='https://github.com/nim65s/django-YummyEmailOrUsernameInsensitiveAuth',
     author='Guilhem Saurel',
     author_email='webmaster@saurel.me',
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Topic :: Internet :: WWW/HTTP',
