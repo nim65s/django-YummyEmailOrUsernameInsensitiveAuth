b76795cf1272d97ec28d88af235e725e78861a88
Guilhem Saurel <guilhem.saurel@gmail.com>
2018-03-18 16:15:28 +0100

    setup.py uses markdown long description

--- setup.py
+++ setup.py
@@ -2,10 +2,7 @@ import os
 
 from setuptools import setup
 
-readme_file = os.path.join(os.path.dirname(__file__), 'README.rst')
-if not os.path.isfile(readme_file):
-    readme_file = os.path.join(os.path.dirname(__file__), 'README.md')
-with open(readme_file) as readme:
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
     README = readme.read()
 
 
@@ -21,6 +18,7 @@ setup(
     license='BSD',
     description='A Django auth backend that works with email or username',
     long_description=README,
+    long_description_content_type="text/markdown",
     url='https://github.com/nim65s/django-YummyEmailOrUsernameInsensitiveAuth',
     author='Guilhem Saurel',
     author_email='webmaster@saurel.me',
